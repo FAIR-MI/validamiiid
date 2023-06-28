af49d55c383bf539b04d140ae8a57b58306d455d
cpauvert <cpauvert@users.noreply.github.com>
2023-06-28 20:16:37 +0200

    fix: missing source for data examples

--- R/invalid_example_miiid.R
+++ R/invalid_example_miiid.R
@@ -3,5 +3,5 @@
 #' One instance of IntermicrobialInteraction object that does not fits the schema
 #'
 #' @format list
-#' @source hand-written
+#' @source based on <https://github.com/FAIR-MI/miiid-schema/blob/790f69ddd6fa8a64232e519b73c68090c64e6041/src/data/examples/invalid/IntermicrobialInteraction-missing-id.yaml>
 "invalid_example_miiid"
--- R/valid_example_miiid.R
+++ R/valid_example_miiid.R
@@ -3,5 +3,5 @@
 #' One instance of IntermicrobialInteraction object that fits the schema
 #'
 #' @format list
-#' @source hand-written
+#' @source <https://github.com/FAIR-MI/miiid-schema/blob/790f69ddd6fa8a64232e519b73c68090c64e6041/src/data/examples/valid/IntermicrobialInteraction-pairwise.yaml>
 "valid_example_miiid"
