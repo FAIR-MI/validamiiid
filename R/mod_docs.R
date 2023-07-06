c1db5a92c06bced067e51d5a017cc69c75f8bb70
cpauvert <cpauvert@users.noreply.github.com>
2023-07-06 10:50:07 +0200

    add help pages and links to tools used

--- R/mod_docs.R
+++ R/mod_docs.R
@@ -18,16 +18,16 @@ mod_docs_ui <- function(id) {
           bslib::card_body(
             tags$ol(
               tags$li(
-                "Start describing interactions between microbial",
+                tags$strong("Describe"), "interactions between microbial",
                 "entities in the editor"
               ),
               tags$li(
-                "Use the (autocompleted) required and",
-                "recommended metadata from MIIID"
+                "Use the (autocompleted)", tags$strong("required"), "and",
+                tags$strong("recommended"), "metadata from MIIID"
               ),
-              tags$li("Or load examples in the editor to play around"),
-              tags$li("Validate the description against the metadata schema"),
-              tags$li("Share your draft description eventually on the MIIID GitHub")
+              tags$li(tags$em("Or load examples in the editor to play around")),
+              tags$li(tags$strong("Validate"), "the description against the metadata schema"),
+              tags$li(tags$strong("Share"), "your draft description eventually on the MIIID GitHub")
             )
           )
         ),
@@ -36,10 +36,19 @@ mod_docs_ui <- function(id) {
           align = "left",
           bslib::card_body(
             tags$p(
-              "It stands for Minimal Information about Intermicrobial Interaction Data (MIIID).",
-              "We",
-              tags$a(href = "https://doi.org/10.1128/msystems.00659-22", "suggested"),
-              "a couple of metadata to achieve this goal."
+              "It stands for",
+              tags$strong("M", .noWS = "after"), "inimal",
+              tags$strong("I", .noWS = "after"), "nformation about",
+              tags$strong("I", .noWS = "after"), "ntermicrobial",
+              tags$strong("I", .noWS = "after"), "nteraction",
+              tags$strong("D", .noWS = "after"), "ata (MIIID),",
+              "and could be developed from",
+              tags$a(
+                href = "https://doi.org/10.1128/msystems.00659-22",
+                "previously suggested metadata",
+                .noWS = "after"
+              ),
+              "."
             ),
             tags$p(
               "This metadata schema is developped using the",
@@ -49,36 +58,58 @@ mod_docs_ui <- function(id) {
                 href = "https://fair-mi.github.io/miiid-schema/",
                 "dedicated MIIID website", .noWS = "after"
               ), "."
-            )
+            ),
+            helpText("Support the MIIID by adding a star to our Github!")
           )
         ),
         bslib::nav_panel(
           "Help",
+          align = "left",
           tags$p(
-            tags$em("Where to find the definition of a specific metadata?"),
-            "In LinkML, they are called _slots_ and can be looked up",
+            tags$strong("Where to find the definition of a specific metadata?"),
+            "In LinkML, they are called", tags$em("slots"), "and can be looked up",
             "on", tags$a(
               href = "https://fair-mi.github.io/miiid-schema/",
               "fair-mi.github.io/miiid-schema", .noWS = "after"
             ), "."
           ),
           tags$p(
-            tags$em("How to fill out the", tags$code("evidence_type")),
+            tags$strong("How to fill out the", tags$code("evidence_type", .noWS = "after"), "?"),
             "Browse to the",
             tags$a(
               href = "https://evidenceontology.org/browse/#ECO_0000000",
               "Evidence & Conclusion Ontology"
             ),
-            "to find an evidence term relevant for your case"
+            "to find an evidence term relevant for your case."
           )
         ),
         bslib::nav_panel(
           icon = shiny::icon("github"), title = "", align = "left",
           bslib::card_body(
             tags$p(
-              tags$code("{validamiiid}"),
-              "is a", tags$code("{shiny}"), "application developped using", tags$code("{golem}"),
-              "It uses the JSON schema artefact exported from the MIIID schema developed with the LinkML framework."
+              tags$a(
+                href = "https://github.com/FAIR-MI/validamiiid",
+                tags$code("{validamiiid}", .noWS = "after")
+              ), "is a",
+              tags$a(
+                href = "https://shiny.posit.co",
+                tags$code("{shiny}", .noWS = "after")
+              ), "application developped using",
+              tags$a(
+                href = "https://thinkr-open.github.io/golem",
+                tags$code("{golem}", .noWS = "after"), .noWS = "after",
+              ), ".",
+              "It uses the",
+              tags$a(
+                href = "https://linkml.io/linkml/generators/json-schema.html",
+                "JSON schema artefact"
+              ),
+              "exported from the MIIID schema (vXXX) developed with the LinkML framework, and",
+              tags$a(
+                href = "https://docs.ropensci.org/jsonvalidate",
+                tags$code("{jsonvalidate}", .noWS = "after"),
+              ),
+              "to perform the validation."
             )
           )
         )
--- dev/02_dev.R
+++ dev/02_dev.R
@@ -25,6 +25,7 @@ golem::add_module(name = "examples_reset", with_test = TRUE) # Name of the modul
 golem::add_module(name = "validate", with_test = TRUE) # Name of the module
 golem::add_module(name = "download", with_test = TRUE) # Name of the module
 golem::add_module(name = "contribute", with_test = TRUE) # Name of the module
+golem::add_module(name = "docs", with_test = FALSE) # Name of the module
 
 
 ## Add helper functions ----
