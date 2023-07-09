3eb4396bf080e658870b23f13682dbd6b78df30b
cpauvert <cpauvert@users.noreply.github.com>
2023-07-09 12:13:51 +0200

    add JSON to YAML conversion function

--- R/fct_conversion.R
+++ R/fct_conversion.R
@@ -1,14 +1,30 @@
 #' YAML-to-JSON conversion
 #'
+#' @param yaml_text A YAML string
+#'
 #' @description This function converts a YAML string and convert into JSON string.
 #'
 #' @return A character vector
 #'
 #' @noRd
-yaml_to_json <- function(yaml_text){
+yaml_to_json <- function(yaml_text) {
   # Convert a text representation of a YAML to JSON string
   # Trying to limit writing files in the shiny app
   yaml::read_yaml(text = yaml_text) %>%
     jsonlite::toJSON(., auto_unbox = TRUE) %>%
     as.character()
+}
+
+#' JSON-to-YAML conversion
+#'
+#' @description converts (and lint) a JSON string vector into YAML string
+#'
+#' @param json_text A character vector of the JSON text
+#'
+#' @return A character vector
+#' @noRd
+json_to_yaml <- function(json_text) {
+  # Convert a text representation of a JSON to a YAML string
+  jsonlite:::fromJSON(json_text) %>%
+    yaml::as.yaml(., indent.mapping.sequence = TRUE)
 }
--- tests/testthat/test-fct_conversion.R
+++ tests/testthat/test-fct_conversion.R
@@ -6,12 +6,24 @@ ex_yaml_pairwise <- paste(c(
   "  - 353",
   "  - 432"
 ), collapse = "\n")
+
 test_that("YAML-to-JSON works", {
-expect_equal(
-  yaml_to_json(""),"{}"
-)
+  expect_equal(
+    yaml_to_json(""), "{}"
+  )
   expect_equal(
     yaml_to_json(ex_yaml_pairwise),
     "{\"participants\":[\"Phocaeicola alice\",\"Phocaeicola bob\"],\"tax_id\":[353,432]}"
+  )
+})
+
+ex_json_pairwise <- '{
+  "participants": ["Phocaeicola alice","Phocaeicola bob"],
+  "tax_id":[353,432]
+}'
+test_that("JSON-to-YAML works", {
+  expect_equal(
+    json_to_yaml(ex_json_pairwise),
+    "participants:\n  - Phocaeicola alice\n  - Phocaeicola bob\ntax_id:\n  - 353\n  - 432\n"
   )
 })
