d87872483373ae651c383098f0498c650d3a7b97
cpauvert <cpauvert@users.noreply.github.com>
2023-07-09 14:06:56 +0200

    add validation procedure to the R6 object

--- R/R6.R
+++ R/R6.R
@@ -2,21 +2,48 @@ InteractionDescription <- R6::R6Class(
   "InteractionDescription",
   public = list(
     default_content = paste(
-        c(
-          "id: miiid: # ID",
-          "participants:",
-          rep("  -  # <PARTICIPANT_NAME>", 3),
-          "tax_id:",
-          rep("  -  # <PARTICIPANT_TAX_ID>", 3),
-          "evidence_type:  # <USING Evidence & Conclusion Ontology (ECO)>",
-          "reference:  # <DOI>"
-        ),
-        collapse = "\n"
+      c(
+        "id: miiid:001_or_example # ID",
+        "participants:",
+        rep("  -  # <PARTICIPANT_NAME>", 3),
+        "tax_id:",
+        rep("  -  # <PARTICIPANT_TAX_ID>", 3),
+        "evidence_type:  # <USING Evidence & Conclusion Ontology (ECO)>",
+        "reference:  # <DOI>"
       ),
-  content = NULL,
-  to_json = function(yaml) {self$content <- yaml_to_json(yaml)},
-  to_yaml = function(json) {self$content <- json_to_yaml(json)},
-  validate = function() {}, # method
-  download = function() {}  # method
+      collapse = "\n"
+    ),
+    is_valid = NULL,
+    error_validation = NULL,
+    content = NULL,
+    # Impossible to assign the validate function as a R6 method
+    # so wrapping a function around to have correct method
+    miiid_validator = function(json) {
+      mv <- jsonvalidate::json_validator(
+        system.file("extdata", "miiid_jsonschema.json", package = "validamiiid"),
+        engine = "ajv"
+      )
+      return(mv(json, verbose = T))
+    },
+    # Validation procedure,
+    # update $content if valid and add $is_valid flag
+    # if not provide an $error_validation and FALSE $is_valid
+    #
+    # TODO: error handling when incomplete id (miiid:SPACE issues a yaml.load error)
+    validate = function(yaml) {
+      # Convert
+      json <- yaml_to_json(yaml)
+      # Validate
+      vld_output <- self$miiid_validator(json)
+      # Update object
+      self$is_valid <- as.logical(vld_output)
+      if (self$is_valid) {
+        self$error_validation <- NULL
+        self$content <- json_to_yaml(json)
+      } else {
+        self$error_validation <- parse_validator(vld_output)
+      }
+      invisible(self)
+    }
   )
 )
